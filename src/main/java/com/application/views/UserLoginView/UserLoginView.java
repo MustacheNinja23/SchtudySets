d004be673e7cd2a71397e21fb152f04235c0d7e5
MustacheNinja23 <87921325+MustacheNinja23@users.noreply.github.com>
2024-12-13 18:43:25 -0500

    fixed bug with server creation

---
+++ src/main/java/com/application/views/UserLoginView/UserLoginView.java
@@ -0,0 +1,57 @@
+package com.application.views.UserLoginView;
+
+import com.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Login Page")
+@Route(value = "", layout = MainLayout.class)
+public class UserLoginView extends HorizontalLayout {
+
+    TextField gameNum, nickName;
+    Button send;
+    VerticalLayout layout;
+
+    public UserLoginView() {
+        gameNum = new TextField();
+        nickName = new TextField();
+        send = new Button("Send");
+
+        gameNum.setPlaceholder("Game Number");
+        nickName.setPlaceholder("Nickname");
+        gameNum.setRequired(true);
+
+        send.addClickListener(e -> {
+            System.out.println(gameNum.getValue());
+            System.out.println(nickName.getValue());
+            UI.getCurrent().navigate("question");
+        });
+        send.addClickShortcut(Key.ENTER);
+
+        Button goToHostView = new Button("help", event -> {
+            UI.getCurrent().navigate("question");
+        });
+
+        this.setSizeFull();
+        this.setVerticalComponentAlignment(Alignment.CENTER);
+        this.setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultVerticalComponentAlignment(Alignment.CENTER);
+
+        layout = new VerticalLayout(Alignment.CENTER, gameNum, nickName, send, goToHostView);
+        layout.setMargin(true);
+        layout.setPadding(true);
+        layout.setSizeFull();
+        layout.setJustifyContentMode(JustifyContentMode.CENTER);
+        layout.setAlignItems(Alignment.CENTER);
+
+        this.add(layout);
+    }
+
+}
